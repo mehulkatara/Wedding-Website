a1d3aead53275bd58cc86b6f1b8688e2579f2214
Mehul Katara <mehulkatara@hotmail.com>
2026-03-18 20:09:54 +0000

    optimize build time

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
+  },
 })
